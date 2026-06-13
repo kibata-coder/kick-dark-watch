c77b51d459d85bb627d5f2e2c86976dca3a015fb
kibata-coder <kibata988@gmail.com>
2026-06-13 23:14:52 +0300

    Add status: 'inprogress' to F1 static matches so cards show 'LIVE' badge instead of 'Upcoming'

--- src/routes/f1.tsx
+++ src/routes/f1.tsx
@@ -9,8 +9,8 @@ const F1_STREAM_URL = "https://streamfree.app/embed/racing/skyf1?server=origin&q
 const LEMANS_STREAM_URL = "https://streamfree.app/embed/racing/yie58g866enp?server=origin&quality=1080p&category=racing";
 
 const staticMatches: Match[] = [
-  { id: "f1-static", title: "Formula 1 Racing", home: { name: "Formula 1 Live", logo: "https://upload.wikimedia.org/wikipedia/commons/3/33/F1.svg" }, away: { name: "Sky Sports F1 HD", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Sky_Sports_F1_-_Logo_2025.svg/1280px-Sky_Sports_F1_-_Logo_2025.svg.png?_=20260323223722" }, league: { name: "Motorsport" } },
-  { id: "lemans-static", title: "24 Hours of Le Mans", home: { name: "WEC Endurance", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/WEC_Logo.svg/500px-WEC_Logo.svg.png?_=20191117125257" }, away: { name: "Le Mans Feed", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/24_Hours_of_Le_Mans_logo_%28since_2014%29.svg/1280px-24_Hours_of_Le_Mans_logo_%28since_2014%29.svg.png?_=20240604043410" }, league: { name: "Endurance Racing" } },
+  { id: "f1-static", title: "Formula 1 Racing", status: "inprogress", home: { name: "Formula 1 Live", logo: "https://upload.wikimedia.org/wikipedia/commons/3/33/F1.svg" }, away: { name: "Sky Sports F1 HD", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Sky_Sports_F1_-_Logo_2025.svg/1280px-Sky_Sports_F1_-_Logo_2025.svg.png?_=20260323223722" }, league: { name: "Motorsport" } },
+  { id: "lemans-static", title: "24 Hours of Le Mans", status: "inprogress", home: { name: "WEC Endurance", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/WEC_Logo.svg/500px-WEC_Logo.svg.png?_=20191117125257" }, away: { name: "Le Mans Feed", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/24_Hours_of_Le_Mans_logo_%28since_2014%29.svg/1280px-24_Hours_of_Le_Mans_logo_%28since_2014%29.svg.png?_=20240604043410" }, league: { name: "Endurance Racing" } },
 ];
 
 function resolveStaticStream(m: Match): string | null {
